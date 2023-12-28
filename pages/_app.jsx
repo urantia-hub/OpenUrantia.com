bf16926f0a9a5b330e73cb36b3b557fcdadf8d66
kelsonic <kgadams93@gmail.com>
2023-12-28 09:43:24 +0100

    Add analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,8 @@
 // Node modules.
+import { Analytics } from "@vercel/analytics/react";
 import { SessionProvider } from "next-auth/react";
-import { useRouter } from "next/router";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { useRouter } from "next/router";
 // Relative modules.
 import "@/styles/globals.css";
 
@@ -15,6 +16,7 @@ export default function App({
         <Component {...pageProps} />
       </SessionProvider>
       {process.env.NODE_ENV === "production" && <SpeedInsights />}
+      {process.env.NODE_ENV === "production" && <Analytics />}
       <script
         async
         src="https://app.termly.io/resource-blocker/2d5a2b8c-1d75-49a7-b386-2dd80780e256?autoBlock=on"
