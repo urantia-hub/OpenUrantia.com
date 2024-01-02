c7132296dd5eba0fd046722d0f2b928f56e6498a
kelsonic <kgadams93@gmail.com>
2024-01-02 20:16:15 +0100

    Update fade in for checkmark

--- pages/papers/[paperId].tsx
+++ pages/papers/[paperId].tsx
@@ -677,11 +677,11 @@ const PaperPage = ({ paperData }: PaperPageProps) => {
                     {node.standardReferenceId}{" "}
                     {readNode && (
                       <span
-                        className="ml-1 text-xs text-gray-400 cursor-help fade-in"
+                        className="ml-1 text-xs text-gray-400 cursor-help"
                         title={readNode ? `You read the paragraph` : ""}
                       >
                         <svg
-                          className="w-2.5 h-2.5 fill-current"
+                          className="w-2.5 h-2.5 fill-current fade-in"
                           viewBox="0 0 122.881 89.842"
                         >
                           <path d="M1.232 55.541a3.746 3.746 0 0 1 5.025-5.554L40.31 80.865l76.099-79.699a3.752 3.752 0 0 1 5.438 5.173L43.223 88.683l-.005-.005a3.746 3.746 0 0 1-5.227.196L1.232 55.541z" />
