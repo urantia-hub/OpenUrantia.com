77d65c08ef0f22a5bb4f6b41286160edc42dff66
kelsonic <kgadams93@gmail.com>
2024-01-02 10:26:01 +0100

    Update adapter types for custom user fields

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,14 @@
 // Node modules.
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth";
+import type { Adapter } from "next-auth/adapters";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export const authOptions = {
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
---
+++ types/next-auth.d.ts
@@ -0,0 +1,11 @@
+// types/next-auth.d.ts
+import { User as NextAuthUser } from "next-auth";
+
+// Extend the built-in types for NextAuth User
+declare module "next-auth" {
+  interface User {
+    lastVisitedGlobalId?: string;
+    lastVisitedPaperId?: string;
+    lastVisitedPaperTitle?: string;
+  }
+}
