5c98f251f399de3efb5d5bf8deebd2e6744a0cb6
kelsonic <kgadams93@gmail.com>
2024-01-05 17:14:03 +0100

    Add More page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,5 @@
 // Node modules.
 import Link from "next/link";
-import { ReadNode } from "@prisma/client";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
@@ -20,7 +19,6 @@ const Navbar = ({ paperId, paperTitle }: NavbarProps) => {
   // State.
   const [lastVisitedNode, setLastVisitedNode] =
     useState<LastVisitedNode | null>(null);
-  const [isSearchOpen, setIsSearchOpen] = useState<boolean>(false);
 
   const fetchLastVisitedNode = async () => {
     try {
@@ -61,25 +59,12 @@ const Navbar = ({ paperId, paperTitle }: NavbarProps) => {
     }
   }, [status]);
 
-  useEffect(() => {
-    // Prevent scrolling on background content when search is open.
-    if (isSearchOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "";
-    }
-  }, [isSearchOpen]);
-
-  const onResetState = () => {
-    setIsSearchOpen(false);
-  };
-
   return (
     <>
       <header className="flex flex-col items-center pt-2 pb-1 px-2 fixed bottom-0 left-0 right-0 z-10 bg-neutral-800 border-t border-neutral-700 mx-auto">
         <PaperNavbar paperId={paperId} paperTitle={paperTitle} />
 
-        <div className="flex items-center justify-around w-full max-w-lg pt-1 pb-2">
+        <div className="flex items-center justify-around w-full max-w-md pt-1 pb-2">
           <Link
             className={`flex-1 flex flex-col items-center text-xs text-center ${
               router.asPath === "/" ? "text-white" : "text-gray-400"
@@ -98,14 +83,14 @@ const Navbar = ({ paperId, paperTitle }: NavbarProps) => {
           </Link>
           <Link
             className={`flex-1 flex flex-col items-center text-xs text-center ${
-              router.asPath.startsWith("/papers")
+              router.asPath.startsWith("/papers/")
                 ? "text-white"
                 : "text-gray-400"
             } line-clamp-1 hover:text-white hover:no-underline transition duration-300 ease-in-out`}
             href={
               status === "authenticated" && lastVisitedNode
                 ? `/papers/${lastVisitedNode.paperId}#${lastVisitedNode.globalId}`
-                : "/papers"
+                : "/papers/0"
             }
           >
             <svg
@@ -115,6 +100,26 @@ const Navbar = ({ paperId, paperTitle }: NavbarProps) => {
               <path d="m12.64 77.27.31-54.92h-6.2v69.88c8.52-2.2 17.07-3.6 25.68-3.66 7.95-.05 15.9 1.06 23.87 3.76a50.968 50.968 0 0 0-16.36-8.88c-7.42-2.42-15.44-3.22-23.66-2.52a3.38 3.38 0 0 1-3.64-3.08c-.02-.2-.02-.39 0-.58zm90.98-57.79c-.02-.16-.04-.33-.04-.51 0-.17.01-.34.04-.51V7.34c-7.8-.74-15.84.12-22.86 2.78-6.56 2.49-12.22 6.58-15.9 12.44V85.9c5.72-3.82 11.57-6.96 17.58-9.1 6.85-2.44 13.89-3.6 21.18-3.02v-54.3zm6.75-3.88h9.14c1.86 0 3.37 1.51 3.37 3.37v77.66a3.372 3.372 0 0 1-4.46 3.19c-9.4-2.69-18.74-4.48-27.99-4.54-9.02-.06-18.03 1.53-27.08 5.52-.56.37-1.23.57-1.92.56-.68.01-1.35-.19-1.92-.56-9.04-4-18.06-5.58-27.08-5.52-9.25.06-18.58 1.85-27.99 4.54-.34.12-.71.18-1.09.18-1.84.01-3.35-1.5-3.35-3.36V18.97c0-1.86 1.51-3.37 3.37-3.37h9.61l.06-11.26a3.366 3.366 0 0 1 2.68-3.28c8.87-1.85 19.65-1.39 29.1 2.23 6.53 2.5 12.46 6.49 16.79 12.25 4.37-5.37 10.21-9.23 16.78-11.72 8.98-3.41 19.34-4.23 29.09-2.8 1.68.24 2.88 1.69 2.88 3.33V15.6h.01zM68.13 91.82c7.45-2.34 14.89-3.3 22.33-3.26 8.61.05 17.16 1.46 25.68 3.66V22.35h-5.77v55.22a3.372 3.372 0 0 1-4.15 3.28c-7.38-1.16-14.53-.2-21.51 2.29-5.62 2.01-11.14 5.01-16.58 8.68zm-10.01-6.57V22.46c-3.53-6.23-9.24-10.4-15.69-12.87-7.31-2.8-15.52-3.43-22.68-2.41l-.38 66.81c7.81-.28 15.45.71 22.64 3.06a57.689 57.689 0 0 1 16.11 8.2z" />
             </svg>
             Read
+          </Link>
+          <Link
+            className={`flex-1 flex flex-col items-center text-xs text-center ${
+              router.asPath.endsWith("/papers") ? "text-white" : "text-gray-400"
+            } line-clamp-1 hover:text-white hover:no-underline transition duration-300 ease-in-out`}
+            href="/papers"
+          >
+            <svg
+              className="w-6 h-6 fill-current mb-1"
+              viewBox="0 0 122.88 78.5"
+            >
+              <path
+                d="M50.57 17.2C40.73 10.31 31 8.79 20.94 11.42c2.2-5.29 5.51-8.76 9.4-10.39C35.85-1.28 44.87.36 48.42 5.3c1.82 2.52 2.73 6.26 2.15 11.9zm32.95 38.12a2.086 2.086 0 1 1 4.06-.96c.54 2.27 1.47 4.03 2.71 5.3 1.27 1.29 2.9 2.12 4.84 2.5 1.13.22 1.86 1.31 1.64 2.44a2.075 2.075 0 0 1-2.44 1.64c-2.76-.55-5.13-1.76-7-3.67-1.79-1.82-3.09-4.22-3.81-7.25zm-69.98 0a2.086 2.086 0 1 1 4.06-.96c.54 2.27 1.47 4.03 2.71 5.3 1.27 1.29 2.9 2.12 4.84 2.5 1.13.22 1.86 1.31 1.64 2.44a2.075 2.075 0 0 1-2.44 1.64c-2.76-.55-5.13-1.76-7-3.67-1.79-1.82-3.09-4.22-3.81-7.25zm47.76-7.61c2.8 0 5.07 2.21 5.07 4.93 0 2.72-2.27 4.93-5.07 4.93-2.8 0-5.07-2.21-5.07-4.93 0-2.72 2.27-4.93 5.07-4.93zM26.66 34.57c10 0 18.11 7.88 18.11 17.61 0 9.72-8.11 17.61-18.11 17.61S8.55 61.91 8.55 52.18s8.1-17.61 18.11-17.61zm69.98 0c10 0 18.11 7.88 18.11 17.61 0 9.72-8.11 17.61-18.11 17.61s-18.11-7.88-18.11-17.61 8.1-17.61 18.11-17.61zM72.31 17.2c9.84-6.89 19.57-8.41 29.62-5.78-2.2-5.29-5.51-8.76-9.4-10.39C87.02-1.28 78 .36 74.46 5.3c-1.82 2.52-2.73 6.26-2.15 11.9zm35.26.2c-3.42-3.52-8.96-5.11-16.63-4.77-8.69.43-17.87 4.06-20.82 12.29-2.47-5.42-14.89-5.42-17.35 0-2.95-8.23-12.13-11.87-20.82-12.29-7.67-.34-13.2 1.26-16.63 4.77C10.5 23.86.96 40.74.16 48.99-3.1 82.47 44.92 90.87 52.62 56.3c4.37 7.75 13.27 7.75 17.64 0 7.7 34.58 55.72 26.17 52.46-7.31-.8-8.25-10.34-25.13-15.15-31.59z"
+                style={{
+                  fillRule: "evenodd",
+                  clipRule: "evenodd",
+                }}
+              />
+            </svg>
+            Discover
           </Link>
           <Link
             className={`flex-1 flex flex-col items-center text-xs text-center ${
@@ -132,12 +137,11 @@ const Navbar = ({ paperId, paperTitle }: NavbarProps) => {
             </svg>
             Search
           </Link>
-          {status !== "authenticated" ? (
+          {status !== "authenticated" && (
             <button
               className="flex-1 flex flex-col border-0 items-center p-0 text-xs text-center text-gray-400 line-clamp-1 hover:text-white hover:no-underline transition duration-300 ease-in-out focus:outline-none"
               onClick={() => {
                 signIn("google", { callbackUrl: "/papers" });
-                onResetState();
               }}
             >
               <svg
@@ -148,59 +152,25 @@ const Navbar = ({ paperId, paperTitle }: NavbarProps) => {
               </svg>
               Sign In
             </button>
-          ) : (
-            <>
-              <Link
-                className={`flex-1 flex flex-col items-center text-xs text-center ${
-                  router.asPath === "/my-library"
-                    ? "text-white"
-                    : "text-gray-400"
-                } line-clamp-1 hover:text-white hover:no-underline transition duration-300 ease-in-out`}
-                href="/my-library"
+          )}
+          {status === "authenticated" && (
+            <Link
+              className={`flex-1 flex flex-col items-center text-xs text-center ${
+                router.asPath.startsWith("/more")
+                  ? "text-white"
+                  : "text-gray-400"
+              } line-clamp-1 hover:text-white hover:no-underline transition duration-300 ease-in-out`}
+              href="/more"
+            >
+              <svg
+                className="w-6 h-6 fill-current mb-1"
+                viewBox="0 0 100 100"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <svg
-                  className="w-6 h-6 fill-current mb-1"
-                  viewBox="0 0 122.88 99.45"
-                >
-                  <path d="M64.48.38H82.9c1.85 0 3.36 1.51 3.36 3.36v14.84H61.12V3.74c0-1.85 1.51-3.36 3.36-3.36zm21.79 27.27v45.19H61.12V27.65h25.15zm0 54.27v13.8c0 1.85-1.51 3.36-3.36 3.36H64.48c-1.85 0-3.36-1.51-3.36-3.36v-13.8h25.15zM101.09.38h18.42c1.85 0 3.36 1.51 3.36 3.36v14.84H97.73V3.74c0-1.85 1.52-3.36 3.36-3.36zm21.79 27.27v45.19H97.73V27.65h25.15zm0 54.27v13.8c0 1.85-1.51 3.36-3.36 3.36H101.1c-1.85 0-3.36-1.51-3.36-3.36v-13.8h25.14zM35.95.2l17.31 6.3c1.74.63 2.64 2.57 2.01 4.31L50.2 24.75l-23.63-8.6 5.07-13.94C32.28.47 34.22-.43 35.95.2zM47.1 33.28 31.65 75.75l-23.63-8.6 15.45-42.46 23.63 8.59zm-18.55 51-4.72 12.96c-.63 1.74-2.57 2.64-4.31 2.01l-17.31-6.3C.47 92.32-.43 90.38.2 88.64l4.72-12.96 23.63 8.6z" />
-                </svg>
-                My Library
-              </Link>
-              <Link
-                className={`flex-1 flex flex-col items-center text-xs text-center ${
-                  router.asPath === "/progress" ? "text-white" : "text-gray-400"
-                } line-clamp-1 hover:text-white hover:no-underline transition duration-300 ease-in-out`}
-                href="/progress"
-              >
-                <svg
-                  className="w-6 h-6 fill-current mb-1"
-                  viewBox="0 0 114.1 122.88"
-                >
-                  <path
-                    d="M75.84 27.1a35.68 35.68 0 0 1 8.61 7.09 32.45 32.45 0 0 1 5.76 9.26 36.84 36.84 0 0 1 1.85 6 34.64 34.64 0 0 1 .24 14 38.69 38.69 0 0 1-2.15 7.32l-.12.25c-2.06 5-5.59 9.86-9 14.66-1.75 2.42-3.48 4.82-4.94 7.15A4.69 4.69 0 0 1 71.73 95l-27.56 4.1A4.7 4.7 0 0 1 39 95.69a40.19 40.19 0 0 0-2.54-5.82 24.85 24.85 0 0 0-3-4.49c-1.43-1.63-2.88-3.29-4.29-5.2A40.42 40.42 0 0 1 25 73.24a41.08 41.08 0 0 1-2.81-8 35.84 35.84 0 0 1-.95-8.45A35.39 35.39 0 0 1 22.35 48a41.69 41.69 0 0 1 3.42-8.85l.2-.35a35.55 35.55 0 0 1 7.13-8.63 33.72 33.72 0 0 1 9.46-5.83l.28-.1a35.41 35.41 0 0 1 8-2.14 37.78 37.78 0 0 1 8.77-.2 39.14 39.14 0 0 1 8.4 1.71 38.44 38.44 0 0 1 7.79 3.49Zm-4 87.26a17.37 17.37 0 0 1-6.28 6.29 16.46 16.46 0 0 1-7.2 2.2A14.87 14.87 0 0 1 51 121.4a15.1 15.1 0 0 1-4.39-3.27l25.29-3.77Zm2.41-14.15v2.23a22 22 0 0 1 0 3.25l-.49 2.39-30.64 4.56-.54-1.23-1.19-4.9v-1.42l32.79-4.88ZM56.34 3.77A3.84 3.84 0 0 1 60.23 0h.27A3.84 3.84 0 0 1 64 3.89a1.27 1.27 0 0 1 0 .2l-.21 8.21a2.11 2.11 0 0 1 0 .26 3.84 3.84 0 0 1-3.87 3.54h-.27a3.84 3.84 0 0 1-3.53-3.88 1.09 1.09 0 0 1 0-.19l.2-8.25ZM14 18.15a3.84 3.84 0 0 1 2.47-6.66 3.83 3.83 0 0 1 2.76 1l6.16 5.73a3.91 3.91 0 0 1 1.22 2.68 3.82 3.82 0 0 1-1 2.76 3.86 3.86 0 0 1-2.67 1.22 3.8 3.8 0 0 1-2.76-1L14 18.15ZM3.92 60.48A3.86 3.86 0 0 1 0 56.75a3.84 3.84 0 0 1 3.73-4l8.41-.28a3.84 3.84 0 0 1 4 3.72v.2a3.84 3.84 0 0 1-3.73 3.77h-.15l-8.3.27Zm106-11.92h.08a3.84 3.84 0 0 1 2.66.86 3.81 3.81 0 0 1 1.4 2.59.49.49 0 0 1 0 .13 3.84 3.84 0 0 1-3.44 4.06l-8.37.89a3.83 3.83 0 0 1-2.81-.85 3.84 3.84 0 0 1 2-6.8c2.79-.31 5.6-.63 8.4-.9ZM93.33 15.09A3.83 3.83 0 0 1 98.65 14a3.73 3.73 0 0 1 1.63 2.44 3.84 3.84 0 0 1-.58 2.88l-4.68 7A3.8 3.8 0 0 1 92.58 28a3.88 3.88 0 0 1-2.88-.57A3.92 3.92 0 0 1 88.06 25a3.84 3.84 0 0 1 .58-2.88l4.69-7Zm-55.1 65.78A42.19 42.19 0 0 1 31 70.56 31.2 31.2 0 0 1 27.89 57a31.7 31.7 0 0 1 3.81-14.44.47.47 0 0 0 .05-.1 27.5 27.5 0 0 1 13.4-11.71 29.65 29.65 0 0 1 13.93-2A32.09 32.09 0 0 1 72.39 33 27.43 27.43 0 0 1 84 46.2a28.85 28.85 0 0 1 0 22.25C81.31 75 75.15 82 71.11 88.4a1.67 1.67 0 0 0-.67 0l-26.08 3.85a35 35 0 0 0-6.13-11.38Z"
-                    style={{
-                      fillRule: "evenodd",
-                    }}
-                  />
-                </svg>
-                Progress
-              </Link>
-              <button
-                className="flex-1 flex flex-col border-0 items-center p-0 text-xs text-center text-gray-400 line-clamp-1 hover:text-white hover:no-underline transition duration-300 ease-in-out"
-                onClick={() => {
-                  signOut();
-                  onResetState();
-                }}
-              >
-                <svg
-                  className="w-6 h-6 fill-current mb-1"
-                  viewBox="0 0 113.055 122.88"
-                >
-                  <path d="M53.114 2.457C53.114 1.1 54.643 0 56.527 0s3.413 1.1 3.413 2.457v44.377c0 1.357-1.528 2.457-3.413 2.457s-3.413-1.1-3.413-2.457V2.457zm20.501 17.204a3.406 3.406 0 0 1-2.026-4.373 3.406 3.406 0 0 1 4.372-2.026c10.962 4.015 20.339 11.339 26.924 20.766a56.262 56.262 0 0 1 10.17 32.325c0 15.606-6.329 29.738-16.559 39.969-10.23 10.229-24.362 16.559-39.969 16.559s-29.739-6.329-39.969-16.559C6.329 96.091 0 81.959 0 66.353a56.261 56.261 0 0 1 10.169-32.325c6.585-9.427 15.962-16.751 26.924-20.766a3.408 3.408 0 0 1 2.346 6.399A49.88 49.88 0 0 0 15.741 37.92c-5.619 8.044-8.916 17.846-8.916 28.433 0 13.723 5.564 26.148 14.559 35.143 8.995 8.995 21.42 14.56 35.143 14.56s26.148-5.564 35.143-14.56c8.995-8.994 14.559-21.42 14.559-35.143 0-10.587-3.297-20.389-8.916-28.433a49.873 49.873 0 0 0-23.698-18.259z" />
-                </svg>
-                Sign Out
-              </button>
-            </>
+                <path d="M10 15h80v10H10zM10 45h80v10H10zM10 75h80v10H10z" />
+              </svg>
+              More
+            </Link>
           )}
         </div>
       </header>
