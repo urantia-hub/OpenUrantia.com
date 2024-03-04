57d8db1b9e9e157114c4a07bbac468ab1353c059
kelsonic <kgadams93@gmail.com>
2024-03-04 09:06:50 +0100

    Add loading state to /papers page

--- pages/papers/index.tsx
+++ pages/papers/index.tsx
@@ -7,6 +7,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import HeadTag from "@/components/HeadTag";
 import { paperLabels } from "@/utils/paperLabels";
+import Spinner from "@/components/Spinner";
 
 // Define the structure of the data you expect from the API
 type TOCNode = {
@@ -31,12 +32,16 @@ const ReadPage = ({ nodes }: TOCPageProps) => {
   // State.
   const [activeFilters, setActiveFilters] = useState<string[]>([]);
   const [showFilters, setShowFilters] = useState<boolean>(false);
-  const [userInterests, setUserInterests] = useState<any[]>([]);
   const [topPapers, setTopPapers] = useState<TOCNode[]>([]);
 
   // Progress state.
   const [progressResults, setProgressResults] = useState<ProgressResult[]>([]);
-  const [fetchingProgress, setFetchingProgress] = useState<boolean>(true);
+  const [fetchingProgress, setFetchingProgress] = useState<boolean>(false);
+
+  // User interests state.
+  const [userInterests, setUserInterests] = useState<any[]>([]);
+  const [fetchingUserInterests, setFetchingUserInterests] =
+    useState<boolean>(true);
 
   useEffect(() => {
     if (userInterests.length > 0) {
@@ -48,24 +53,32 @@ const ReadPage = ({ nodes }: TOCPageProps) => {
   }, [userInterests, nodes, progressResults]);
 
   const fetchUserInterests = async () => {
-    const response = await fetch(`/api/user/interests`);
-    const data = await response.json();
+    try {
+      setFetchingUserInterests(true);
+      const response = await fetch(`/api/user/interests`);
+      const data = await response.json();
 
-    // Check if the user has interests, if they have been redirected before, or if they skipped the selection
-    if (
-      data.userInterests?.length === 0 &&
-      !sessionStorage.getItem("redirectedToInterests") &&
-      !sessionStorage.getItem("skippedInterestsSelection")
-    ) {
-      sessionStorage.setItem("redirectedToInterests", "true");
-      window.location.href = "/onboarding/interests"; // Redirect to the onboarding interests selection page
-    } else {
-      setUserInterests(data.userInterests);
+      // Check if the user has interests, if they have been redirected before, or if they skipped the selection
+      if (
+        data.userInterests?.length === 0 &&
+        !sessionStorage.getItem("redirectedToInterests") &&
+        !sessionStorage.getItem("skippedInterestsSelection")
+      ) {
+        sessionStorage.setItem("redirectedToInterests", "true");
+        window.location.href = "/onboarding/interests"; // Redirect to the onboarding interests selection page
+      } else {
+        setUserInterests(data.userInterests);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setFetchingUserInterests(false);
     }
   };
 
   const fetchProgress = async () => {
     try {
+      setFetchingProgress(true);
       const response = await fetch("/api/user/nodes/progress", {
         method: "POST",
         headers: {
@@ -388,6 +401,12 @@ const ReadPage = ({ nodes }: TOCPageProps) => {
     .filter((node) => node.type === "part")
     .sort((a, b) => parseInt(a.partId) - parseInt(b.partId));
 
+  // Loading state.
+  const isLoading =
+    (fetchingProgress && status === "authenticated") ||
+    (fetchingUserInterests && status === "authenticated") ||
+    status === "loading";
+
   return (
     <div className="flex flex-col min-h-screen bg-neutral-800">
       <HeadTag
@@ -398,161 +417,174 @@ const ReadPage = ({ nodes }: TOCPageProps) => {
       <Navbar />
 
       <main className="mt-8 flex-grow container mx-auto px-4 my-4 max-w-4xl">
-        <div className="mt-4 mb-4 text-center">
-          <h1 className="text-5xl font-bold mb-8">The Urantia Papers</h1>
+        {isLoading ? (
+          <div className="mt-4 mb-4 text-center">
+            <h1 className="text-5xl font-bold mb-8">The Urantia Papers</h1>
+            <Spinner />
+          </div>
+        ) : (
+          <>
+            <div className="mt-4 mb-4 text-center">
+              <h1 className="text-5xl font-bold mb-8">The Urantia Papers</h1>
 
-          {/* -- Papers You Might Like --- */}
-          {topPapers.length > 0 && (
-            <div className="mb-8">
-              <h2 className="text-sm mb-2 pb-2 text-center border-b text-gray-400 border-gray-600">
-                Papers You Might Like
-              </h2>
+              {/* -- Papers You Might Like --- */}
+              {topPapers.length > 0 && (
+                <div className="mb-8">
+                  <h2 className="text-sm mb-2 pb-2 text-center border-b text-gray-400 border-gray-600">
+                    Papers You Might Like
+                  </h2>
 
-              <p className="text-xs text-gray-400 mb-6">
-                (Suggestions based on{" "}
-                <Link
-                  className="text-orange-400 hover:underline"
-                  href="/onboarding/interests"
-                >
-                  your interests
-                </Link>
-                )
-              </p>
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {topPapers.map((paper) => {
-                  // Find the progress result for the current paper and derive its completion status.
-                  const progressResult = progressResults.find(
-                    (progressResult) => progressResult.paperId === paper.paperId
-                  );
-                  const isCompleted = progressResult?.progress === 100;
-                  const isNotStarted = progressResult?.progress === 0;
-                  const progressClasses = isNotStarted
-                    ? "bg-zinc-600"
-                    : isCompleted
-                    ? "bg-gradient-to-r from-purple-400 via-pink-500 to-red-500"
-                    : "bg-white";
-
-                  return (
+                  <p className="text-xs text-gray-400 mb-6">
+                    (Suggestions based on{" "}
                     <Link
-                      key={paper.globalId}
-                      href={`/papers/${paper.paperId}`}
-                      className="flex flex-col items-start text-left justify-between px-4 py-2 mb-2 bg-neutral-700 rounded hover:bg-neutral-600 transition-colors hover:no-underline"
+                      className="text-orange-400 hover:underline"
+                      href="/onboarding/interests"
                     >
-                      <div className="flex flex-col w-full">
-                        <div className="text-xs text-gray-400 flex items-center justify-between w-full">
-                          {paper.paperId === "0" ? (
-                            "Foreword"
-                          ) : (
-                            <>
-                              <span>Paper {paper.paperId}</span>{" "}
-                              <span>Part {paper.partId}</span>
-                            </>
-                          )}
-                        </div>
-                        <h3 className="mt-1 text-lg font-bold leading-6">
-                          {paper.paperTitle}
-                        </h3>
-                      </div>
-                      <div className="flex flex-col w-full">
-                        <span
-                          className="mt-1 text-xs text-gray-400 truncate w-full"
-                          title={paper.labels.sort().join(" | ")}
-                          dangerouslySetInnerHTML={{
-                            __html: highlightUserInterestLabels(
-                              paper.labels,
-                              userInterests
-                            )
-                              .sort()
-                              .join(" | "),
-                          }}
-                        />
-                        {/* Progress */}
-                        {progressResult && (
-                          <div className="flex flex-col mt-1 w-full">
-                            <div className="bg-zinc-600 rounded-full h-2.5 w-full relative mb-1">
-                              <div
-                                className={`absolute h-2.5 rounded-full ${progressClasses}`}
-                                style={{ width: `${progressResult.progress}%` }}
-                              />
+                      your interests
+                    </Link>{" "}
+                    and reading progress.)
+                  </p>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {topPapers.map((paper) => {
+                      // Find the progress result for the current paper and derive its completion status.
+                      const progressResult = progressResults.find(
+                        (progressResult) =>
+                          progressResult.paperId === paper.paperId
+                      );
+                      const isCompleted = progressResult?.progress === 100;
+                      const isNotStarted = progressResult?.progress === 0;
+                      const progressClasses = isNotStarted
+                        ? "bg-zinc-600"
+                        : isCompleted
+                        ? "bg-gradient-to-r from-purple-400 via-pink-500 to-red-500"
+                        : "bg-white";
+
+                      return (
+                        <Link
+                          key={paper.globalId}
+                          href={`/papers/${paper.paperId}`}
+                          className="flex flex-col items-start text-left justify-between px-4 py-2 mb-2 bg-neutral-700 rounded hover:bg-neutral-600 transition-colors hover:no-underline"
+                        >
+                          <div className="flex flex-col w-full">
+                            <div className="text-xs text-gray-400 flex items-center justify-between w-full">
+                              {paper.paperId === "0" ? (
+                                "Foreword"
+                              ) : (
+                                <>
+                                  <span>Paper {paper.paperId}</span>{" "}
+                                  <span>Part {paper.partId}</span>
+                                </>
+                              )}
                             </div>
-                            {progressResult.progress < 100 && (
-                              <div className="text-xs">
-                                Continue Reading{" "}
-                                {progressResult.progress < 100 ? (
-                                  <>
-                                    {" "}
-                                    <span className="text-gray-400">
-                                      ({progressResult.progress.toFixed(0)}%)
-                                    </span>
-                                  </>
-                                ) : (
-                                  ""
+                            <h3 className="mt-1 text-lg font-bold leading-6">
+                              {paper.paperTitle}
+                            </h3>
+                          </div>
+                          <div className="flex flex-col w-full">
+                            <span
+                              className="mt-1 text-xs text-gray-400 truncate w-full"
+                              title={paper.labels.sort().join(" | ")}
+                              dangerouslySetInnerHTML={{
+                                __html: highlightUserInterestLabels(
+                                  paper.labels,
+                                  userInterests
+                                )
+                                  .sort()
+                                  .join(" | "),
+                              }}
+                            />
+                            {/* Progress */}
+                            {progressResult && (
+                              <div className="flex flex-col mt-1 w-full">
+                                <div className="bg-zinc-600 rounded-full h-2.5 w-full relative mb-1">
+                                  <div
+                                    className={`absolute h-2.5 rounded-full ${progressClasses}`}
+                                    style={{
+                                      width: `${progressResult.progress}%`,
+                                    }}
+                                  />
+                                </div>
+                                {progressResult.progress < 100 && (
+                                  <div className="text-xs">
+                                    Continue Reading{" "}
+                                    {progressResult.progress < 100 ? (
+                                      <>
+                                        {" "}
+                                        <span className="text-gray-400">
+                                          ({progressResult.progress.toFixed(0)}
+                                          %)
+                                        </span>
+                                      </>
+                                    ) : (
+                                      ""
+                                    )}
+                                  </div>
+                                )}
+                                {progressResult.progress === 100 && (
+                                  <div className="text-green-400 text-xs">
+                                    Completed (100%)
+                                  </div>
                                 )}
                               </div>
                             )}
-                            {progressResult.progress === 100 && (
-                              <div className="text-green-400 text-xs">
-                                Completed (100%)
-                              </div>
-                            )}
                           </div>
-                        )}
-                      </div>
-                    </Link>
-                  );
-                })}
-              </div>
-            </div>
-          )}
+                        </Link>
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
 
-          {/* -- All Papers --- */}
-          <h2 className="text-sm pb-2 text-center border-b text-gray-400 border-gray-600">
-            All Papers
-          </h2>
+              {/* -- All Papers --- */}
+              <h2 className="text-sm pb-2 text-center border-b text-gray-400 border-gray-600">
+                All Papers
+              </h2>
 
-          {/* Render filter toggle buttons */}
-          {showFilters ? (
-            <>
-              <button
-                className="px-3 py-1 rounded text-sm md:text-xs font-semibold bg-neutral-600 text-neutral-300 mt-4"
-                onClick={() => {
-                  setShowFilters(false);
-                  setActiveFilters([]);
-                }}
-              >
-                Hide Filters
-              </button>
-              <div className="flex flex-wrap gap-2 mt-4 mb-4">
-                {paperLabels.map((label) => (
+              {/* Render filter toggle buttons */}
+              {showFilters ? (
+                <>
                   <button
-                    key={label}
-                    className={`px-3 py-1 rounded text-sm md:text-xs font-semibold ${
-                      activeFilters.includes(label)
-                        ? "bg-blue-600 text-white"
-                        : "bg-neutral-600 text-neutral-300"
-                    }`}
-                    onClick={() => toggleFilter(label)}
+                    className="px-3 py-1 rounded text-sm md:text-xs font-semibold bg-neutral-600 text-neutral-300 mt-4"
+                    onClick={() => {
+                      setShowFilters(false);
+                      setActiveFilters([]);
+                    }}
                   >
-                    {label}
+                    Hide Filters
                   </button>
-                ))}
-              </div>
-            </>
-          ) : (
-            <div className="flex justify-center mt-4">
-              <button
-                className="px-3 py-1 rounded text-sm md:text-xs font-semibold bg-neutral-600 text-neutral-300"
-                onClick={() => setShowFilters(true)}
-              >
-                Filter by Topics
-              </button>
+                  <div className="flex flex-wrap gap-2 mt-4 mb-4">
+                    {paperLabels.map((label) => (
+                      <button
+                        key={label}
+                        className={`px-3 py-1 rounded text-sm md:text-xs font-semibold ${
+                          activeFilters.includes(label)
+                            ? "bg-blue-600 text-white"
+                            : "bg-neutral-600 text-neutral-300"
+                        }`}
+                        onClick={() => toggleFilter(label)}
+                      >
+                        {label}
+                      </button>
+                    ))}
+                  </div>
+                </>
+              ) : (
+                <div className="flex justify-center mt-4">
+                  <button
+                    className="px-3 py-1 rounded text-sm md:text-xs font-semibold bg-neutral-600 text-neutral-300"
+                    onClick={() => setShowFilters(true)}
+                  >
+                    Filter by Topics
+                  </button>
+                </div>
+              )}
             </div>
-          )}
-        </div>
 
-        {/* Render parts and papers */}
-        {foreword && renderNode(foreword)}
-        {sortedNodes.map((node) => renderNode(node))}
+            {/* Render parts and papers */}
+            {foreword && renderNode(foreword)}
+            {sortedNodes.map((node) => renderNode(node))}
+          </>
+        )}
       </main>
       <Footer />
     </div>
