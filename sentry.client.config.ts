9e2f673b0d408fcf6a88f57fff02cba48b4a91aa
kelsonic <kgadams93@gmail.com>
2024-03-15 11:31:28 +0100

    Update sentry to only capture errors on prod

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,35 +4,37 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://3e482dce99f521fbb29aeda8001efd2a@o4506857923739648.ingest.us.sentry.io/4506857924984832",
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://3e482dce99f521fbb29aeda8001efd2a@o4506857923739648.ingest.us.sentry.io/4506857924984832",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-  replaysOnErrorSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
 
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-  integrations: [
-    Sentry.replayIntegration({
-      // Additional Replay configuration goes in here, for example:
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-    // Sentry.feedbackIntegration({
-    //   colorScheme: "dark",
-    //   showBranding: false,
-    //   themeDark: {
-    //     background: "#000",
-    //     border: "#000",
-    //   },
-    // }),
-  ],
-});
+    // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+    integrations: [
+      Sentry.replayIntegration({
+        // Additional Replay configuration goes in here, for example:
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
+      // Sentry.feedbackIntegration({
+      //   colorScheme: "dark",
+      //   showBranding: false,
+      //   themeDark: {
+      //     background: "#000",
+      //     border: "#000",
+      //   },
+      // }),
+    ],
+  });
+}
