a42979b24412c4df65956b97b98f1e5fdb42ce20
kelsonic <kgadams93@gmail.com>
2024-05-18 16:43:18 +0200

    Add papers 3 and 4

--- pages/papers/[paperId].tsx
+++ pages/papers/[paperId].tsx
@@ -17,11 +17,13 @@ import Spinner from "@/components/Spinner";
 import TopReadingNavbar from "@/components/TopReadingNavbar";
 
 const AUDIO_ENABLED = true;
-const AUDIO_ENABLED_PAPER_IDS = ["0", "1", "2"];
+const AUDIO_ENABLED_PAPER_IDS = ["0", "1", "2", "3", "4"];
 const PAPER_ID_TO_MP3_URL = {
-  "0": `${process.env.NEXT_PUBLIC_URANTIA_DEV_API_HOST}/data/mp3/eng/tts-1-hd-nova-`,
-  "1": `${process.env.NEXT_PUBLIC_URANTIA_DEV_API_HOST}/data/mp3/eng/tts-1-hd-nova-`,
-  "2": `${process.env.NEXT_PUBLIC_URANTIA_DEV_API_HOST}/data/mp3/eng/tts-1-hd-nova-`,
+  "0": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
+  "1": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
+  "2": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
+  "3": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
+  "4": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
 };
 const AVERAGE_READING_SPEED = 400; // Words per minute
 const NEXT_AUDIO_DELAY = 300; // Milliseconds
