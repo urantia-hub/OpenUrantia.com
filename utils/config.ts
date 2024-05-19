210de56e084ccee8a3fa4dbb0a4ee3e6ac49ee71
kelsonic <kgadams93@gmail.com>
2024-05-19 09:45:18 +0200

    Add papers

--- utils/config.ts
+++ utils/config.ts
@@ -17,6 +17,13 @@ export const AUDIO_ENABLED_PAPER_IDS = [
   "12",
   "13",
   "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
 ];
 export const PAPER_ID_TO_MP3_URL = {
   "0": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
@@ -34,6 +41,13 @@ export const PAPER_ID_TO_MP3_URL = {
   "12": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
   "13": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
   "14": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
+  "15": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
+  "16": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
+  "17": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
+  "18": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
+  "19": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
+  "20": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
+  "21": `${process.env.NEXT_PUBLIC_AUDIO_FILES_CDN}/tts-1-hd-nova-`,
 };
 export const NEXT_AUDIO_DELAY = 300; // Milliseconds
 
