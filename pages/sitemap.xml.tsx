a777e31b5772ef4a94f787fa343a8e9c1f5c4b14
kelsonic <kgadams93@gmail.com>
2024-07-06 14:02:49 +0200

    Update sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,4 +1,4 @@
-import { getValidPaperUrls } from "@/utils/paperFormatters";
+import { getValidPaperUrls, paperIdToUrl } from "@/utils/paperFormatters";
 
 const Sitemap = () => {
   // This page will not be rendered directly, so return null
@@ -14,39 +14,38 @@ export const getServerSideProps = async ({ res }: any) => {
     { url: `${baseUrl}/explore`, priority: "0.9" },
     { url: `${baseUrl}/papers`, priority: "0.9" },
     { url: `${baseUrl}/search`, priority: "0.7" },
-    { url: `${baseUrl}/auth/sign-in`, priority: "0.3" },
-    { url: `${baseUrl}/auth/error`, priority: "0.3" },
-    { url: `${baseUrl}/auth/sign-out`, priority: "0.3" },
-    { url: `${baseUrl}/auth/verify-request`, priority: "0.3" },
-    { url: `${baseUrl}/privacy-policy`, priority: "0.3" },
-    { url: `${baseUrl}/terms-of-service`, priority: "0.3" },
-    { url: `${baseUrl}/cookie-policy`, priority: "0.3" },
+    { url: `${baseUrl}/auth/sign-in`, priority: "0.1" },
+    { url: `${baseUrl}/auth/error`, priority: "0.1" },
+    { url: `${baseUrl}/auth/sign-out`, priority: "0.1" },
+    { url: `${baseUrl}/auth/verify-request`, priority: "0.1" },
+    { url: `${baseUrl}/privacy-policy`, priority: "0.1" },
+    { url: `${baseUrl}/terms-of-service`, priority: "0.1" },
+    { url: `${baseUrl}/cookie-policy`, priority: "0.1" },
   ];
 
+  // Add all paper URLs.
   const validPaperUrls = getValidPaperUrls();
-
   const papers = validPaperUrls.map((url) => ({
     url: `${baseUrl}/papers/${url}`,
     priority: "0.8",
   }));
 
-  const urls = [...staticPages, ...papers].sort(
-    (a, b) =>
-      // Sort by priority
-      parseFloat(b.priority) - parseFloat(a.priority)
+  // Combine all URLs and sort by priority.
+  const urls = [...staticPages, ...papers].sort((a, b) =>
+    a.priority > b.priority ? -1 : 1
   );
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls
-      .map(
-        (url) => `<url>
-          <loc>${url.url}</loc>
-          <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
-          <priority>${url.priority}</priority>
-        </url>`
-      )
-      .join("")}
+  ${urls
+    .map(
+      (url) => `<url>
+        <loc>${url.url}</loc>
+        <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
+        <priority>${url.priority}</priority>
+      </url>`
+    )
+    .join("")}
   </urlset>`;
 
   res.setHeader("Content-Type", "text/xml");
