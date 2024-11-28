2c6185e78fe6609ad33121698c6d0cccabeee4a5
kelsonic <kgadams93@gmail.com>
2024-11-27 23:58:37 -0600

    Add blockchain archive link

--- next.config.js
+++ next.config.js
@@ -1,57 +1,47 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa");
+const { withSentryConfig } = require("@sentry/nextjs");
 
-// module.exports = withPWA({
-//   dest: "public",
-//   register: true,
-//   skipWaiting: true,
-// });
-
-module.exports = {
+const nextConfig = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
+
+  // Add redirects configuration
+  async redirects() {
+    return [
+      {
+        source: "/blockchain-archive/urantia-papers/json",
+        destination:
+          "https://3ujo66t434cz5na2kozifk52b3mvweqts2k5ievkggxdcjkoqpja.arweave.net/3RLvenzfBZ60GlOygqu6DtlbEhOWldQSqjGuMSVOg9I",
+        permanent: true,
+      },
+    ];
+  },
 };
 
-// Injected content via Sentry wizard below
+// Apply PWA configuration
+// const withPWAConfig = withPWA({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+// })(nextConfig);
 
-const { withSentryConfig } = require("@sentry/nextjs");
-
+// Apply Sentry configuration
 module.exports = withSentryConfig(
-  module.exports,
+  nextConfig,
   {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
     silent: true,
     org: "openurantia",
     project: "javascript-nextjs",
   },
   {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
     transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
     tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
     hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
     automaticVercelMonitors: true,
   }
 );
