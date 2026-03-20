ed9cc349af5d6e6f5903f6c9424f19c2cccae139
kelsonic <kgadams93@gmail.com>
2026-03-20 17:35:42 -0500

    Update logo

--- components/LogoSymbol.tsx
+++ components/LogoSymbol.tsx
@@ -1,12 +1,12 @@
 const LogoSymbol = ({ className }: { className?: string }) => (
   <svg className={`${className || ""}`} viewBox="0 0 512 512">
-    <circle cx="256" cy="256" r="200.00" fill="none" stroke="currentColor" strokeWidth="1.60" opacity="0.10"/>
-    <circle cx="256" cy="256" r="166.60" fill="none" stroke="currentColor" strokeWidth="1.60" opacity="0.15"/>
-    <circle cx="256" cy="256" r="133.40" fill="none" stroke="currentColor" strokeWidth="2.00" opacity="0.22"/>
-    <circle cx="256" cy="256" r="100.00" fill="none" stroke="currentColor" strokeWidth="3.00" opacity="0.40"/>
-    <circle cx="256" cy="256" r="66.60" fill="none" stroke="currentColor" strokeWidth="5.00" opacity="0.70"/>
-    <circle cx="256" cy="256" r="33.40" fill="none" stroke="currentColor" strokeWidth="7.00" opacity="0.95"/>
-    <circle cx="256" cy="256" r="10.40" fill="currentColor"/>
+    <circle cx="256" cy="256" r="248" fill="none" stroke="currentColor" strokeWidth="12.8" opacity="0.25"/>
+    <circle cx="256" cy="256" r="208" fill="none" stroke="currentColor" strokeWidth="12.8" opacity="0.35"/>
+    <circle cx="256" cy="256" r="168" fill="none" stroke="currentColor" strokeWidth="14.4" opacity="0.50"/>
+    <circle cx="256" cy="256" r="128" fill="none" stroke="currentColor" strokeWidth="16.0" opacity="0.65"/>
+    <circle cx="256" cy="256" r="88" fill="none" stroke="currentColor" strokeWidth="16.0" opacity="0.80"/>
+    <circle cx="256" cy="256" r="51.2" fill="none" stroke="currentColor" strokeWidth="17.6" opacity="0.95"/>
+    <circle cx="256" cy="256" r="22.4" fill="currentColor"/>
   </svg>
 );
 
